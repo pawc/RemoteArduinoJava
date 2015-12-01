ed62fb54babbefa8736b1422c34dd9510499b253
pawc <pswid89@gmail.com>
2015-12-01 09:34:04 +0100

    implementing frame for remote client

--- RemoteClient/src/main/java/pl/pawc/arduino/Main.java
+++ RemoteClient/src/main/java/pl/pawc/arduino/Main.java
@@ -1,18 +1,58 @@
 package pl.pawc.arduino;
 
+import pl.pawc.arduino.controller.Controller;
+
+import javafx.application.Application; 
+import javafx.application.Platform; 
+import javafx.event.EventHandler; 
+import javafx.fxml.FXMLLoader; 
+import javafx.scene.Scene; 
+import javafx.scene.layout.AnchorPane; 
+import javafx.scene.layout.BorderPane; 
+import javafx.stage.Stage; 
+import javafx.stage.WindowEvent;
+
 import java.io.IOException;
 
-import pl.pawc.arduino.client.ServerConnection;
+public class Main extends Application {
 
-public class Main {
-    public static void main( String[] args ){
-        try{
-        int i = Integer.parseInt(args[1]);
-        ServerConnection serverConnection = new ServerConnection(i, args[0]);
-        serverConnection.run();
-        }
-        catch(NumberFormatException | ArrayIndexOutOfBoundsException | IOException e){
-        e.printStackTrace();
-        }
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Platform.runLater(new Runnable() {
+            public void run(){
+
+                Controller controller = new Controller();
+                FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("Frame.fxml"));
+                loader.setController(controller);
+                AnchorPane anchorPane = null;
+                    try {
+                        anchorPane = (AnchorPane) loader.load();
+                    }
+                    catch(IOException e){
+                         e.printStackTrace();
+                    }
+
+                primaryStage.setTitle("Controlls");
+                Scene scene = new Scene(anchorPane);
+                primaryStage.setResizable(false);
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            }
+
+        });
+    
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>(){
+            @Override
+            public void handle(WindowEvent event) {
+                System.exit(0);
+            }            
+        });
+
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
---
+++ RemoteClient/src/main/java/pl/pawc/arduino/controller/Controller.java
@@ -0,0 +1,12 @@
+package pl.pawc.arduino.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
+public class Controller{
+
+    @FXML Button connectButton;
+    @FXML RadioButton LEDswitch;
+
+}
